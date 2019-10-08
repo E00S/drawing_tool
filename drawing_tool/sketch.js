cbb88dc3a8f4e588cbd82f579b7bb14c8b00c908
Leon Rogge <37003958+E00S@users.noreply.github.com>
2019-10-08 11:21:12 -0700

    Added Stuff

---
+++ drawing_tool/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+
+    let canvas = createCanvas(800, 800);
+    canvas.parent("p5")
+}
+
+function draw() {
+	line(mouseX, mouseY, mouseX - 10, mouseY - 10);
+}
+
+function mousePressed() {
+
+}
+
+function mouseReleased() {
+
+}
+
+function colorBlue() {
+
+}
